2e0a63f9b237d6642ebf9477ba95162cfc24071a
travelfleek <roque.buarque@gmail.com>
2022-05-29 21:32:35 +0200

    fake data

--- feature/stocks/src/main/java/com/roquebuarque/smartstocks/stocks/data/StockRepositoryImpl.kt
+++ feature/stocks/src/main/java/com/roquebuarque/smartstocks/stocks/data/StockRepositoryImpl.kt
@@ -21,20 +21,20 @@ internal class StockRepositoryImpl @Inject constructor(val retrofit: Retrofit) :
 
         fun mock(): List<StockDto> {
             return listOf(
-                StockDto("US0378331005", price = Random.nextDouble()),
-                StockDto("DE000A1EWWW0", price = Random.nextDouble()),
-                StockDto("DE0007100000", price = Random.nextDouble()),
-                StockDto("US64110L1061", price = Random.nextDouble()),
-                StockDto("US6541061031", price = Random.nextDouble()),
-                StockDto("US0231351067", price = Random.nextDouble()),
-                StockDto("DE0007236101", price = Random.nextDouble()),
-                StockDto("DE000BAY0017", price = Random.nextDouble()),
-                StockDto("DE0007664005", price = Random.nextDouble()),
-                StockDto("US88160R1014", price = Random.nextDouble()),
-                StockDto("NL0000009165", price = Random.nextDouble()),
-                StockDto("XS1168962063", price = Random.nextDouble()),
-                StockDto("US90353T1007", price = Random.nextDouble()),
-                StockDto("US7960542030", price = Random.nextDouble())
+                StockDto("US0378331005", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("DE000A1EWWW0", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("DE0007100000", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("US64110L1061", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("US6541061031", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("US0231351067", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("DE0007236101", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("DE000BAY0017", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("DE0007664005", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("US88160R1014", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("NL0000009165", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("XS1168962063", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("US90353T1007", price = Random.nextDouble(from = 10.0, until = 100.0)),
+                StockDto("US7960542030", price = Random.nextDouble(from = 10.0, until = 100.0))
             )
         }
     }
